ee4f524f2ca893e20b514d79afe0f7aac64743c7
Mu Shen <shenmu0710@163.com>
2022-03-18 21:13:17 +0800

    feat: 404优化

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,3 +1,37 @@
+<script lang="ts" setup>
+import { useDirect } from '@/utils/helper';
+const { redirect, routerBack } = useDirect();
+</script>
 <template>
-    <div>404</div>
+    <div class="ct">
+        <div class="code">404</div>
+        <div class="text">当前页面无法访问</div>
+        <div class="buttons">
+            <el-button @click="routerBack">返回上一页</el-button>
+            <el-button @click="redirect('/user_center')">返回个人主页</el-button>
+        </div>
+    </div>
 </template>
+
+<style lang="less" scoped>
+.ct {
+    height: calc(100vh - 60px - 30px);
+    background-color: #002d54;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+.code {
+    color: #fff;
+    font-size: 160px;
+    letter-spacing: 0.1em;
+}
+.text {
+    color: #fff;
+    font-size: 20px;
+    letter-spacing: 0.1em;
+}
+.buttons {
+    margin: 30px 0;
+}
+</style>
