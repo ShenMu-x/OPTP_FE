a9fa687445e3861626d68f42bde0a57062965dfa
Mu Shen <shenmu0710@163.com>
2022-07-03 23:02:59 +0800

    fix: 修复ts报错

--- src/pages/manage/tab/CoursePanel.vue
+++ src/pages/manage/tab/CoursePanel.vue
@@ -6,25 +6,21 @@ import Avatar from '@/components/common/Avatar.vue';
 import EditCoursePanel from './courseComp/EditCoursePanel.vue';
 import LoadBtn from '@/components/common/LoadBtn.vue';
 import { useReloader } from '@/utils/helper';
-import { getAllCourse, courseType } from '@/utils/services';
+import { getAllCourse, manageCourseType } from '@/utils/services';
 import { CodeLangs } from '@/utils/option';
 import { emptyCourse } from './default';
 
 const refTableEl = ref();
 const refEditEl = ref();
-const course = ref<courseType>(emptyCourse);
+const course = ref<manageCourseType>({ ...emptyCourse });
 const editInfoHandler = (params: any) => {
     course.value = params;
     refEditEl?.value?.openPanel?.();
-}
+};
 const submitHandler = () => {
-    refTableEl?.value?.reload?.()
-}
-const {
-    isReloading,
-    reloadHandler,
-    finishReload,
-} = useReloader(refTableEl);
+    refTableEl?.value?.reload?.();
+};
+const { isReloading, reloadHandler, finishReload } = useReloader(refTableEl);
 </script>
 
 <template>
@@ -35,8 +31,14 @@ const {
             </template>
         </BtnCt>
         <EditCoursePanel ref="refEditEl" :course="course" @submit="submitHandler" />
-        <TablePage :page-size="6" :fetch-data="getAllCourse" ref="refTableEl" emptyDes="系统中暂无课程"
-            @reloadend="finishReload" :no-tip="false">
+        <TablePage
+            :page-size="6"
+            :fetch-data="getAllCourse"
+            ref="refTableEl"
+            emptyDes="系统中暂无课程"
+            @reloadend="finishReload"
+            :no-tip="false"
+        >
             <template v-slot:tableColumns>
                 <el-table-column label="头像" min-width="60">
                     <template #default="scope">
@@ -52,13 +54,17 @@ const {
                     <template #default="scope">{{ scope.row.secretKey }}</template>
                 </el-table-column>
                 <el-table-column label="状态" min-width="80">
-                    <template #default="scope">{{ scope.row.isClosed ? '已关闭' : '进行中' }}</template>
+                    <template #default="scope">
+                        {{ scope.row.isClosed ? '已关闭' : '进行中' }}
+                    </template>
                 </el-table-column>
                 <el-table-column prop="createdAt" label="创建时间" min-width="100" />
                 <el-table-column prop="updatedAt" label="更新时间" min-width="100" />
                 <el-table-column label="操作" min-width="120">
                     <template #default="scope">
-                        <el-button type="danger" size="default" @click="editInfoHandler(scope.row)">修改信息</el-button>
+                        <el-button type="danger" size="default" @click="editInfoHandler(scope.row)">
+                            修改信息
+                        </el-button>
                     </template>
                 </el-table-column>
             </template>
